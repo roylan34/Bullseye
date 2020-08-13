428b3766dec1829486b0270c22699823b71d79f7
Roelan Eroy <roelan@vanana.com>
2020-08-13 21:53:00 +0800

    implement webView in About view

--- Bullseye/AboutViewController.swift
+++ Bullseye/AboutViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
+   
+   
+   @IBOutlet weak var webView: WKWebView!
 
    override func viewDidLoad() {
       super.viewDidLoad()
-      
+      renderWebView()
    }
    
    @IBAction func close(){
       dismiss(animated: true, completion: nil)
+   }
+   
+   func renderWebView(){
+      if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html"){
+         let url = URL(fileURLWithPath: htmlPath);
+         let request = URLRequest(url: url);
+         webView.load(request);
+      }
    }
    
 
